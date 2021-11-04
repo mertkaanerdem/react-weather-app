8e95fc7e5f24d609fb2b4fc1332f0abeba57c2e3
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-11-04 21:08:43 +0300

    cities are coming

--- src/components/Cities/Cities.jsx
+++ src/components/Cities/Cities.jsx
@@ -3,9 +3,18 @@ import { useContext } from "react";
 import WeatherContext from "../../context/WeatherContext";
 
 function Cities() {
-  const { cities_list } = useContext(WeatherContext);
+  const { cities_list, selectedCity, setSelectedCity } =
+    useContext(WeatherContext);
+
+  const handleCityChange = (e) => {
+    for (let i = 0; i < cities_list.length; i++) {
+      if (e.target.value !== cities_list[i].il_adi) {
+        setSelectedCity(cities_list[i]);
+      }
+    }
+  };
   return (
-    <select>
+    <select value={selectedCity.il_adi} onChange={handleCityChange}>
       {cities_list.map((city, i) => (
         <option key={i} value={city.il_adi}>
           {city.il_adi}
